1bcb1fa2aeca50ba96e1f754e52330094577ea5f
Fenice <187117889+Fen1ce@users.noreply.github.com>
2024-11-02 17:55:23 +0100

    Modifiche index.html
    index.html: eliminato CSS per background dal codice (già presente in file CSS per sito) e corretto alcuni URL di href
    
    Gorak_Shan: modificata funzione changeImage() tramite aggiunta del parametro id
    
    D&D_Site-javascript.js: modifica della funzione changeImage() tramite aggiunta del parametro id per futura compatibilità con altre pagine dei personaggi

--- D&D_Site-javascript.js
+++ D&D_Site-javascript.js
@@ -1,22 +1,22 @@
 // FUNZIONE CAMBIO IMMAGINE
-function changeImage() {
-    var image = document.getElementById('Gorak_Shan');
-    if (image.src.match("/Figures/Gorak_Shan_propic.jpg")) {
-        image.src = "/Figures/Gorak_Shan_token.png";
-    } else {
-        image.src = "/Figures/Gorak_Shan_propic.jpg";
+function changeImage(id) { // cambia ciclicamente l'immagine visualizzata
+    var image = document.getElementById(id);
+    if (image.src.match('Figures/'+id+'_propic.jpg')) { // se l'immagine è una propic
+        image.src = "Figures/"+id+"_token.png"; // la sostituisce con il token
+    } else { // se invece l'immagine è un token
+        image.src = "Figures/"+id+"_propic.jpg"; // la sostituisce con la propic
     }
 }
 
 // FUNZIONE INDICE A SCOMPARSA
 function cambiaStato (id){ // cambia ciclicamente la visibilità dell'elemento indicato
-if (document.getElementById){ // se un elemento con l'identificativo specificato esiste,
-	if(document.getElementById(id).style.display == 'none') { // se l'elemento è nascosto,
-		document.getElementById(id).style.display = 'block'; // lo visualizza
-		document.getElementById('index_btn').innerHTML = "Nascondi indice"; // aggiorna l'etichetta del pulsante, invertendo l'azione compiuta
-	}else{ // se invece l'elemento è visualizzato,
-		document.getElementById(id).style.display = 'none'; // lo nasconde
-		document.getElementById('index_btn').innerHTML = "Mostra indice"; // aggiorna l'etichetta del pulsante, ripristinando quella originale
-} // fine controllo stato
-} // fine controllo presenza elemento
+	if (document.getElementById){ // se un elemento con l'identificativo specificato esiste,
+		if(document.getElementById(id).style.display == 'none') { // se l'elemento è nascosto,
+			document.getElementById(id).style.display = 'block'; // lo visualizza
+			document.getElementById('index_btn').innerHTML = "Nascondi indice"; // aggiorna l'etichetta del pulsante, invertendo l'azione compiuta
+		}else{ // se invece l'elemento è visualizzato,
+			document.getElementById(id).style.display = 'none'; // lo nasconde
+			document.getElementById('index_btn').innerHTML = "Mostra indice"; // aggiorna l'etichetta del pulsante, ripristinando quella originale
+		} // fine controllo stato
+	} // fine controllo presenza elemento
 } // fine funzione cambiaStato
